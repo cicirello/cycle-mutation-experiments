5988b420e2a97ffa269d66574778f104c041c988
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2022-05-29 14:07:47 -0400

    documentation updates

--- src/main/java/org/cicirello/experiments/cyclemutation/LCSExperimentsStronglyRegularSA.java
+++ src/main/java/org/cicirello/experiments/cyclemutation/LCSExperimentsStronglyRegularSA.java
@@ -32,8 +32,14 @@ import org.cicirello.search.ProgressTracker;
 import org.cicirello.search.operators.permutations.PermutationInitializer;
 
 /**
- * Experiments with the Largest Common Subgraph Problem and with Simulated Annealing,
- * with a strongly regular instance of the LCS.
+ * <p>Experiments with the Largest Common Subgraph Problem and with Simulated Annealing,
+ * with a strongly regular instance of the LCS.</p>
+ *
+ * <p>Code to reproduce the experiments from the following article:</p>
+ *
+ * <p>Vincent A. Cicirello. 2022. Cycle Mutation: Evolving Permutations via Cycle Induction. 
+ * <i>Applied Sciences</i>, 12, 11, Article 5506 (May 2022). 
+ * <a href="https://doi.org/10.3390/app12115506">https://doi.org/10.3390/app12115506</a></p>
  *
  * @author <a href=https://www.cicirello.org/ target=_top>Vincent A. Cicirello</a>, 
  * <a href=https://www.cicirello.org/ target=_top>https://www.cicirello.org/</a>
